5f2b91256550ce17c5b20985b8e76483022e40b2
bluestarburst <hargreaves.bryant@gmail.com>
2022-03-10 14:30:32 -0600

    l

--- src/elements/sources.js
+++ src/elements/sources.js
@@ -6,13 +6,13 @@ import Header from "./header"
 export default function Sources(props) {
     return (<>
         <Header static />
-        <div className="sources">
+        <div className="sources" style={{paddingTop: "15vh", paddingBottom: "5vh"}}>
             <h2>Works Cited</h2>
             <br></br>
             <p><i>Bilingual Beowulf - MIT</i>. <a target="_blank" href="https://www.mit.edu/~jrising/webres/beowulf.pdf">https://www.mit.edu/~jrising/webres/beowulf.pdf</a>. </p>
-            <p>Bondar, Vladimir. "Aoristic Drift and Narrative Perfect in Early Modern English: A Functional Approach." International Journal of English Studies, vol. 21, no. 2, July 2021, pp. 57+. Gale Academic OneFile, <a target="_blank" href="link.gale.com/apps/doc/A689557274/AONE?u=txshracd2560&sid=bookmark-AONE&xid=864b8a87">link.gale.com/apps/doc/A689557274/AONE?u=txshracd2560&sid=bookmark-AONE&xid=864b8a87</a>. Accessed 2 Mar. 2022.</p>
+            <p>Bondar, Vladimir. "Aoristic Drift and Narrative Perfect in Early Modern English: A Functional Approach." International Journal of English Studies, vol. 21, no. 2, July 2021, pp. 57+. Gale Academic OneFile, <a target="_blank" href="https://link.gale.com/apps/doc/A689557274/AONE?u=txshracd2560&sid=bookmark-AONE&xid=864b8a87">link.gale.com/apps/doc/A689557274/AONE?u=txshracd2560&sid=bookmark-AONE&xid=864b8a87</a>. Accessed 2 Mar. 2022.</p>
             <p>Crystal, David. “Middle English.” British Library, 31 Jan. 2018, <a target="_blank" href="https://www.bl.uk/medieval-literature/articles/middle-english">https://www.bl.uk/medieval&#8209;literature/articles/middle&#8209;english</a>. </p>
-            <p>Ellis, Sian. "Journey into Anglo-Saxon England." British Heritage, vol. 33, no. 2, May 2012, pp. 38+. Gale OneFile: World History, <a target="_blank" href="link.gale.com/apps/doc/A286718124/PPWH?u=txshracd2560&sid=bookmark-PPWH&xid=4dfdfe5e">link.gale.com/apps/doc/A286718124/PPWH?u=txshracd2560&sid=bookmark-PPWH&xid=4dfdfe5e</a>. Accessed 2 Mar. 2022.</p>
+            <p>Ellis, Sian. "Journey into Anglo-Saxon England." British Heritage, vol. 33, no. 2, May 2012, pp. 38+. Gale OneFile: World History, <a target="_blank" href="https://link.gale.com/apps/doc/A286718124/PPWH?u=txshracd2560&sid=bookmark-PPWH&xid=4dfdfe5e">link.gale.com/apps/doc/A286718124/PPWH?u=txshracd2560&sid=bookmark-PPWH&xid=4dfdfe5e</a>. Accessed 2 Mar. 2022.</p>
             <p>Robinson, Bonnie J. and Getty, Laura, "British Literature I: Middle Ages to the Eighteenth Century and Neoclassicism" (2018). English Open Textbooks. 17.
                 <a target="_blank" href="https://oer.galileo.usg.edu/english-textbooks/17">https://oer.galileo.usg.edu/english&#8209;textbooks/17</a>.
             </p>
